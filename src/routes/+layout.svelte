077ce7dd5109b2c0ed84f4e50f76ba302e1f6efd
Johnathan Ludwig <john@straiker.ai>
2026-03-31 15:31:52 -0400

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { updated } from '$app/state';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
