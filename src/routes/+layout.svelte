7c78c189ab7e0a8cee42718a998472771467dc08
Johnathan Ludwig <john@straiker.ai>
2026-03-31 15:32:11 -0400

    Add version check

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,32 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+
+	let toast = $state()
+	$effect(() => {
+		if (updated.current) {
+			toast= 'A new update is available.'
+		}
+	});
+
+	function onvisibilitychange() {
+		if (document.visibilityState === 'visible') {
+			updated.check();
+		}
+	}
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<svelte:document {onvisibilitychange} />
+
 {@render children()}
+
+{#if toast}
+	<div class="">
+		{toast}
+		<button onclick={() => location.reload()}>Refresh</button>
+	</div>
+{/if}
